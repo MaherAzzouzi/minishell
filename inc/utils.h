c9dc3fd6e52d234278ff32e5898283d9020350d9
Saleh Nagat <snagat@e3r1p4.1337.ma>
2022-06-11 13:07:44 +0100

    fixing double free

--- inc/utils.h
+++ inc/utils.h
@@ -6,5 +6,6 @@ int is_even(int num);
 int is_odd(int num);
 void ignore_spaces(t_lnode **head);
 void free_tree(t_parsing_node *root);
+int count_token(t_lnode* head, t_lnode *current, e_token t);
 
 # endif
