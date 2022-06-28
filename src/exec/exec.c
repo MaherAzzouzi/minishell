9fa23fd991d3b335f166a35e7242e1e56f501673
Saleh Nagat <snagat@e3r7p4.1337.ma>
2022-06-28 13:35:03 +0100

    cat | ls works for now

--- src/exec/exec.c
+++ src/exec/exec.c
@@ -11,11 +11,27 @@ char *return_cmd_full_path(t_parsing_node *root, t_exec_struct *exec_s)
     return p;
 }
 
-pid_t spawn_process(int in, int out, t_parsing_node *root, t_exec_struct *exec_s)
+void ft_close(int fd, int p)
+{
+    if (p)
+        write(1, "PARENT :", 9);
+    else
+        write(1, "CHILD :", 8);
+    printf("%d\n", fd);
+    close(fd);
+}
+
+void ft_pipe(int *fd)
+{
+    pipe(fd);
+    printf("pipe : %d %d\n", fd[0], fd[1]);
+}
+
+pid_t spawn_process(int in, int out, t_parsing_node *root, t_exec_struct *exec_s, int *fd)
 {
     pid_t pid;
 
-    printf("Executing %s\n", root->cmd.cmd);
+    //printf("Executing %s\n", root->cmd.cmd);
     pid = fork();
     if (pid == 0)
     {
@@ -24,26 +40,30 @@ pid_t spawn_process(int in, int out, t_parsing_node *root, t_exec_struct *exec_s
         if (in != 0)
         {
             dup2(in, 0);
-            close(in);
+           // ft_close(in, 0);
         }
 
         if (out != 1)
-        {
+        { 
             dup2(out, 1);
-            close(out);
+           // ft_close(out, 0);
         }
         p = return_cmd_full_path(root ,exec_s);
         if (p == NULL)
             show_errno();
+        ft_close(fd[0], 0);
+        ft_close(fd[1], 0);
         execve(p, root->cmd.argv, exec_s->envp);
         exit(0);
+       
     }
     else
     {
+        //    waitpid(c, 0, 0);
         if (in != 0)
-            close(in);
+            ft_close(in, 1);
         if (out != 1)
-            close(out);
+            ft_close(out, 1);
         return (pid);
     }
 }
@@ -57,113 +77,54 @@ void show_content(int fd)
     write(1, p, ret);
 }
 
-// void recursive_exec(t_parsing_node *node, t_exec_struct *exec_s, int out)
-// {
-//     int fd[2];
-//     static int f;
-//     int pid1;
-//     int pid2;
-//     int status;
-
-//     pid1 = 0;
-//     pid2 = 0;
-//     printf("type = %s\n", convert_token(node->type));
-//     if (node->type == PIPE)
-//     {
-//         pipe(fd);
-//         printf("Here\n");
-//         if (node->lchild->type == CMD && node->rchild->type == CMD)
-//         {
-//             if (f == 0)
-//             {
-//                 pid1 = spawn_process(0, fd[1], node->rchild, exec_s);
-//                 printf("pid1 %d\n", pid1);
-//                 f = 1;
-//             }
-//             pid2 = spawn_process(fd[0], out, node->lchild, exec_s);
-//             printf("pid2 %d\n", pid2);
-//             waitpid(pid2, &status, 0);
-//             if (f == 0)
-//                 waitpid(pid1, &status, 0);
-//             printf("END\n");
-//             node->type = CMD;
-//             return ;
-//         }
-//     }
-//     printf("Here2\n");
-//     recursive_exec(node->rchild, exec_s, fd[1]);
-//     printf("Here3\n");
-//     recursive_exec(node, exec_s, out);
-// }
-
-// void recursive_exec(t_parsing_node *node, t_exec_struct *exec_s)
-// {
-//     static int fd[2];
-//     //int status;
-//     // int pid1;
-//     // int pid2;
-
-//     if (node->type == PIPE)
-//     {
-//         pipe(fd);
-//         spawn_process(0, fd[1], node->lchild, exec_s);
-//         wait(NULL);
-//         dup2(fd[0], 0);
-
-//         pipe(fd);
-//         node = node->rchild;
-//         spawn_process(0, fd[1], node->lchild, exec_s);
-//         spawn_process(fd[0], 1, node->rchild, exec_s);
-
-//         node = node->rchild;
-        
-
-//         wait(NULL);
-//     }
-//     //show_content(fd[1]);
-// }
-
-void save_old_io()
-{
-    int fd[2];
-
-    fd[0] = dup(0);
-    fd[1] = dup(1);
-}
-
-void restore_io()
-{
-
-}
-
 void recursive_exec(t_parsing_node *node, t_exec_struct *exec_s)
 {
     static int fd[2];
-    //int status;
-    // int pid1;
-    // int pid2;
+    int status;
+    int pid1;
+    int pid2;
     int fd2;
 
     fd2 = dup(0);
     while (node->type == PIPE)
     {
         pipe(fd);
-        spawn_process(0, fd[1], node->lchild, exec_s);
-
+        pid1 = spawn_process(0, fd[1], node->lchild, exec_s,fd);
         if (node->rchild->type == CMD)
         {
-            spawn_process(fd[0], 1, node->rchild, exec_s);
+            pid2 = spawn_process(fd[0], 1, node->rchild, exec_s,fd);
+            // waitpid(pid1, &status, 0);
+            waitpid(pid2, &status, 0);
         }
         else
         {
             dup2(fd[0], 0);
+            close(fd[0]);
         }
-        wait(NULL);
         node = node->rchild;
     }
     dup2(fd2, 0);
     close(fd2);
+    while ( waitpid(-1, &status, 0) != -1 )
+       ;
 }
+
+
+
+// void recursive_exec(t_parsing_node *node, t_exec_struct *exec_s)
+// {
+//     // int pid;
+//     // int status;
+//     int fd[2];
+
+//     if (node->type == PIPE)
+//     {
+//         ft_pipe(fd);
+//         spawn_process(0, fd[1], node->lchild, exec_s,fd);
+//         //waitpid(pid, &status, 0);
+//         spawn_process(fd[0], 1, node->rchild, exec_s,fd);
+//     }
+// }
 
 void execute(t_parsing_node *root, t_exec_struct *exec_s, char *envp[])
 {
