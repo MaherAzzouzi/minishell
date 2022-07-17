27c0525da5f884952e9b2d23f209398cd3517d52
MaherAzzouzi <maherazz04@gmail.com>
2022-07-17 15:14:31 +0100

    Adding gitignore

--- src/exec/or_and_exec.c
+++ src/exec/or_and_exec.c
@@ -7,7 +7,7 @@ void exec_and_or(t_parsing_node *root, t_exec_struct *exec_s, char **envp)
 	execute(root->lchild, exec_s, envp);
 	if (root->type == AND)
 	{
-		if (WIFEXITED(exec_s->exit_status) && WEXITSTATUS(exec_s->exit_status) == 0)
+		if (WIFEXITED(exec_s->exit_status) && WEXITSTATUS(exec_s->exit_status) == 0)1
 			execute(root->rchild, exec_s, envp);
 	}
 	else
