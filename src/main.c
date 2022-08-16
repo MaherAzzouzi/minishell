9243b2def81f97e705ff694f604883804ead22b7
MaherAzzouzi <maherazz04@gmail.com>
2022-08-16 18:01:29 +0100

    We will try to merge everything now

--- src/main.c
+++ src/main.c
@@ -37,9 +37,10 @@ void loop_handler(char *envp[], t_exec_struct* exec_s);
 void ctrl_c_handler(int p)
 {
 	(void)p;
-	write(1, "HH", 2);
-	//loop_handler(g_envp, g_exec_struct);
-	return;
+	if (g_exec_struct->exit_status == 0)
+		printf("\n" GREEN "$PWNAI> " WHITE);
+	else
+		printf("\n" RED "$PWNAI> " WHITE); 
 }
 
 void loop_handler(char *envp[], t_exec_struct* exec_s)
@@ -51,50 +52,13 @@ void loop_handler(char *envp[], t_exec_struct* exec_s)
 	}
 }
 
-struct termios ts;
-
-void reset_the_terminal()
-{
-	tcsetattr(0, 0, &ts);
-}
-
-void handle_the_stuff(int num)
-{
-char buff[4];
-buff[0] = '[';
-buff[2] = '0' + num%10;
-num /= 10;
-buff[1] = '0' + num%10;
-buff[3] = ']';
-write(0, buff, sizeof buff);
-}
-
-
 int	main(int argc, char *argv[], char *envp[])
 {
 	(void)argc;
 	(void)argv;
 	t_exec_struct exec_struct;
-	int ret;
-	
-	struct termios tn;
-	rl_catch_signals = 0;
-
-	ret = tcgetattr(0, &ts);
-	if (ret) {printf("ERROR!");}
-
-	atexit(reset_the_terminal);
-	tn = ts;
-
-	tn.c_lflag &= ~ECHOCTL;
-
-	ret = tcsetattr(0, 0, &tn);
-	if (ret) {printf("ERROR!");}
 	signal(SIGINT, ctrl_c_handler);
 
-	printf("(pseudoshell)Start typing:\n" );
-//while(1) {getc(stdin);}
-//	exit (0);
 	g_exec_struct = &exec_struct;
 	g_envp = envp;
 
