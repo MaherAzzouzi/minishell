a9b96ed6991da03462186a9c2f50d3771edf0588
Saleh Nagat <snagat@e3r7p5.1337.ma>
2022-08-29 20:37:04 +0100

    norm builtins part1

--- src/exec/builtins/cd_utils.c
+++ src/exec/builtins/cd_utils.c
@@ -1,19 +1,17 @@
 #include "minishell.h"
 
-void update_env(t_envp **en, char *str, char *path)
+void	update_env(t_envp **en, char *str, char *path)
 {
-	t_envp *current;
-	char *fr;
+	t_envp	*current;
+	char	*fr;
 
 	fr = NULL;
 	current = *en;
 	while (current)
 	{
-		//printf("CMP %s,%s\n", current->str, str);
 		if (!ft_strncmp(current->str, str, ft_strlen(str)))
 		{
 			free(current->str);
-			//getchar();
 			fr = ft_strjoin(str, "=", 3);
 			current->str = ft_strjoin(fr, path, 0);
 		}
--- src/exec/builtins/env.c
+++ src/exec/builtins/env.c
@@ -1,65 +1,82 @@
-
 #include "minishell.h"
 
-int ft_env(t_parsing_node *node, t_exec_struct *exec_s, t_envp **env)
+typedef struct s_var_env
 {
-	(void)exec_s;
-	t_envp *head;
+	t_envp	*head;
 	t_envp	*current;
 	char	*s;
 	char	*var;
-	int	i;
-	int	flag;
+	int		i;
+	int		flag;
+}	t_var_env;
 
-	flag = 0;
-	head = *env;
-	if (node->cmd.argv[1] == NULL)
+static void	print_env(t_var_env *var, t_envp **env)
+{
+	var->head = *env;
+	while (var->head)
 	{
-		while(head)
-		{
-			if(ft_egale_len(head->str) != -1)
-				fprintf(stdout,"%s\n", head->str);
-			head = head->next;
-		}
+		if (ft_egale_len(var->head->str) != -1)
+			printf("%s\n", var->head->str);
+		var->head = var->head->next;
 	}
+}
+
+static void	replace_env_var(t_var_env *var, t_envp **env, t_parsing_node *node)
+{
+	var->current = *env;
+	while (var->current)
+	{
+		var->var = ft_strndup(var->current->str,
+				ft_egale_len(var->current->str));
+		if (!ft_strcmp(var->s, var->var)
+			&& var->current->str[ft_egale_len(var->current->str)] == '=')
+		{
+			var->flag = 1;
+			free(var->current->str);
+			var->current->str = ft_strdup(node->cmd.argv[var->i]);
+		}
+		free(var->var);
+		var->current = var->current->next;
+	}
+	free(var->s);
+}
+
+static int	loop_for_newenv(t_var_env *var, t_parsing_node *node, t_envp **env)
+{
+	var->i = 1;
+	var->flag = 0;
+	while (node->cmd.argv[var->i])
+	{
+		if (ft_egale_len(node->cmd.argv[var->i]) != -1
+			&& node->cmd.argv[var->i][0] != '=')
+			var->s = ft_strndup(node->cmd.argv[var->i],
+					ft_egale_len(node->cmd.argv[var->i]));
+		else
+		{
+			ft_putstr_fd("No such file or directory\n", 2);
+			exit_status_fail();
+			return (-1);
+		}
+		replace_env_var(var, env, node);
+		if (var->flag == 0)
+			new_env(env, node->cmd.argv[var->i]);
+		var->i++;
+	}
+	return (0);
+}
+
+int	ft_env(t_parsing_node *node, t_envp **env)
+{
+	t_var_env	var;
+
+	if (node->cmd.argv[1] == NULL)
+		print_env(&var, env);
 	else
 	{
-		i = 1;
-		while (node->cmd.argv[i])
-		{
-			if (ft_egale_len(node->cmd.argv[i]) != -1 && node->cmd.argv[i][0] != '=')
-				s = ft_strndup(node->cmd.argv[i], ft_egale_len(node->cmd.argv[i]));
-			else
-			{
-				fprintf(stderr, "No such file or directory\n");
-				exit_status_fail();
-				return(-1);
-			}
-			current = *env;
-			while(current)
-			{
-				var = ft_strndup(current->str, ft_egale_len(current->str));
-				if (!ft_strcmp(s, var) && current->str[ft_egale_len(current->str)] == '=')
-				{
-					flag = 1;
-					free(current->str);
-					current->str = ft_strdup(node->cmd.argv[i]);
-				}
-				free(var);
-				current = current->next;
-			}
-			free(s);
-			if (flag == 0)
-				new_env(env, node->cmd.argv[i]);
-			i++;	
-		}
-		while(head)
-		{
-			if(ft_egale_len(head->str) != -1)
-				fprintf(stdout,"%s\n", head->str);
-			head = head->next;
-		} 
+		if (loop_for_newenv(&var, node, env) == -1)
+			return (-1);
+		print_env(&var, env);
 	}
 	exit_status_success();
-	return 0;
+	return (0);
 }
